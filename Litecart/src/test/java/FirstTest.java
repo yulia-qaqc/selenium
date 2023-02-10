0446683db581d4376b9a9ad6462b93e9f8bd37c8
Yulia Shishkina <y.sh@mail.ru>
2023-02-10 16:05:55 +0300

    Update FirstTest.java

--- Litecart/src/test/java/FirstTest.java
+++ Litecart/src/test/java/FirstTest.java
@@ -1,22 +1,20 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
 import java.util.List;
-
 public class FirstTest extends MainClass {
     // Сценарий проверяет, что у каждого товара имеется ровно один стикер
     @Test
     public void stickersTest() {
         driver.get("http://localhost/litecart/en/");
         List<WebElement> items = driver.findElements(By.cssSelector("div.image-wrapper"));
-        Object[] itemsArray = items.toArray();
-        for (int i = 0; i < itemsArray.length; i++) {
+        int num = items.size();
+        for (int i = 0; i < num; i++) {
             (items.get(i)).click();
             List<WebElement> stickers = driver
                     .findElements(By.cssSelector("div[id='box-product']  div[class*='sticker']"));
-            Object[] stickersArray = stickers.toArray();
-            if (stickersArray.length == 1) {
+            int x = stickers.size();
+            if (x == 1) {
                 System.out.println("У товара один стикер");
             }
             else {System.out.println("Ошибка");
