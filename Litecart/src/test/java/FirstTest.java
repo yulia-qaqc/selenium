dce3ed784b71291ff3c049c3ab82952ad283d980
Yulia Shishkina <y.sh@mail.ru>
2023-02-13 12:25:00 +0300

    Update FirstTest.java

--- Litecart/src/test/java/FirstTest.java
+++ Litecart/src/test/java/FirstTest.java
@@ -8,14 +8,14 @@ public class FirstTest extends MainClass {
     public void stickersTest() {
         driver.get("http://localhost/litecart/en/");
         List<WebElement> items = driver
-                .findElements(By.cssSelector("li[class='product column shadow hover-light'] div.image-wrapper"));
+                .findElements(By.cssSelector("ul.products li"));
         int num = items.size();
         for (int i = 0; i < num; i++) {
             List<WebElement> stickers = items.get(i)
-                    .findElements(By.cssSelector("li[class='product column shadow hover-light'] div.image-wrapper div[class*='sticker']"));
+                    .findElements(By.cssSelector("div.sticker"));
             int stickersNum = stickers.size();
             if (stickersNum == 1) {
-                System.out.println((i + 1) + " товар: найден один стикер");
+                System.out.println((i + 1) + " товар: один стикер");
             } else {
                 System.out.println((i + 1) + " товар: ошибка");
             }
